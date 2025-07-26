eaa8e44af537606e4c00c9c2c955c6a5888094ba
Vipul Garg <vgarg1603@gmail.com>
2025-07-26 19:50:31 +0530

    Ready to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -12,6 +13,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -23,6 +26,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
